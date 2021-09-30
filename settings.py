dff2b63154d93287c6b5c03ead2ba7839fa68075
Anand Sanghani <>
2021-09-30 16:52:49 +0100

    remove comment on example cluster_url

--- settings.py
+++ settings.py
@@ -2,4 +2,4 @@
 DB_NAME = 'fiservTest'
 
 # cluster connection string
-# CLUSTER_URL = f'mongodb+srv://user:password@something.mongodb.net/{DB_NAME}?retryWrites=true&w=majority'
+CLUSTER_URL = f'mongodb+srv://user:password@something.mongodb.net/{DB_NAME}?retryWrites=true&w=majority'
