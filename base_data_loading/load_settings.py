9faa99bd6d19bbc6d2c17515ef7c1333d98d1bcb
Anand Sanghani <>
2021-10-10 15:21:18 +0100

    Change DB name

--- base_data_loading/load_settings.py
+++ base_data_loading/load_settings.py
@@ -1,5 +1,5 @@
 # Database name to use
-DB_NAME = 'fiserv'
+DB_NAME = 'fiservTest'
 
 # cluster connection string
 CLUSTER_URL = f'mongodb+srv://user:password@something.mongodb.net/{DB_NAME}?retryWrites=true&w=majority'
