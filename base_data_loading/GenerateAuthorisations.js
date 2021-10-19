b969e4d27b63544776b734d9d50808170cb613e3
Anand Sanghani <>
2021-10-19 18:42:07 +0100

    Latest changes

--- base_data_loading/GenerateAuthorisations.js
+++ base_data_loading/GenerateAuthorisations.js
@@ -8,141 +8,166 @@
  */
 
 //  var chance = require('chance').Chance();
- var mgenerate = require('mgeneratejs');
- const { EJSON } = require('bson');
- var MongoClient = require('mongodb').MongoClient;
+var fs = require('fs');
+var mgenerate = require('mgeneratejs');
+const { EJSON } = require('bson');
+const BATCH_SIZE=100
+var MongoClient = require('mongodb').MongoClient;
+var URI
+var numberOfDocs
+var client
+var db
+var fileName
  
- function generateAuth() {
-     return mgenerate(
-        {
-            "auth_id": "$objectid",
-            "merch_id": {"$number": {"min": 30001, "max": 30002}},
-            "alliance_code" : "CODE123",
-            "bank_name": "bank_name",
-            "bank_country": "$country",
-            "posting_date" : {"$date": {"min": "2020-01-01", "max": "2020-12-31"}},
-            "merchant_details": {
-                "external_merch_id": {"$number": {"min": 100000, "max": 1000000}},
-                "custom_merch_id": {"$number": {"min": 100000, "max": 1000000}},
-                "merch_type": {"$string": {"length": 6, "symbols": false, "alpha": true, "casing": "upper"}},
-                "name": "$company",
-                "trade_name": "$company",
-                "mcc_iso": {"$number": {"min": 100000, "max": 1000000}},
-                "partner_code": {"$join": {"array": [{"$string": {"length": 3, "symbols": false, "alpha": true, "casing": "upper"}}, {"$number": {"min":10000, "max":20000}}]}}
-            },
-            "agent_code": {"$number": {"min": 100000, "max": 1000000}},
-            "platform_code": {"$number": {"min": 100000, "max": 1000000}},
-            "instrument_details": {
-                "instrument_num": {"$number": {"min": 100000, "max": 1000000}},
-                "instrument_num_masked": {"$number": {"min": 1000000000000000, "max": 9000000000000000}},
-                "instrument_type_code" : {"$string": {"length": 3, "symbols": false, "alpha": true, "casing": "upper"}},
-                "instrument_type_subcode" : {"$string": {"length": 3, "symbols": false, "alpha": true, "casing": "upper"}}
-            },
-            "auth_details": {
-                "auth_code" : {"$string": {"length": 9, "symbols": false, "alpha": true, "casing": "upper"}},
-                "auth_date" : {"$date": {"min": "2020-01-01", "max": "2020-12-31"}},
-                "auth_exp" : {"$date": {"min": "2020-01-01", "max": "2020-12-31"}},
-                "auth_flag": "AA",
-                "auth_id": "$objectid",
-                "auth_ref_id": {"$number": {"min": 100000, "max": 1000000}},
-                "auth_seq": {"$number": {"min": 1, "max": 10}},
-                "auth_time": {"$join": {"array": [{"$number": {"min":0, "max":24}},":",{"$number": {"min":0, "max":60}},":",{"$number": {"min":0, "max":60}}]}},
-                "auth_method": {"$choose": {"from": ["ABC","123","XYZ"]}},
-                "auth_method_code": {"$number": {"min": 1021, "max": 2131}},
-                "auth_platform_code": {"$number": {"min": 1021, "max": 2131}}
-            },
-            "acquirer_details" : {
-                "merch_id": {"$number": {"min": 901000, "max": 901030}},
-                "resp_code": {"$number": {"min": 9001, "max": 9999}},
-                "resp_code_desc": "$string",
-                "terminal_id": {"$number": {"min": 301000, "max": 301030}},
-                "acquired_id": {"$number": {"min": 401000, "max": 401030}},
-                "bin": {"$string": {"length": 4, "symbols": false, "alpha": true, "casing": "upper"}},
-                "token": "$objectid"
-            },
-            "account_details": {
-                "account_type": {"$choose": {"from": ["Credit","Debit","chequing"],"weights": [10, 10, 1]}},
-                "account_type_code": {"$choose": {"from": ["CR1213","DB1212","CQ1213"],"weights": [10, 10, 1]}},
-                "account_update_service_flag": {"$string": {"length": 2, "symbols": false, "alpha": true, "casing": "upper"}}
-            },
-            "card_details" : {
-                "card_classification" : {"$string": {"length": 6, "symbols": false, "alpha": true, "casing": "mixed"}},
-                "card_curr_code": {"$choose": {"from": ["USD","GBP","EUR"],"weights": [1,10,10] }},
-                "card_holder_name": "$name",
-                "card_service_type_code": {"$string": {"length": 4, "symbols": false, "alpha": true, "casing": "upper"}},
-                "card_usage_code": {"$string": {"length": 4, "symbols": false, "alpha": true, "casing": "upper"}},
-                "card_verification": {"$string": {"length": 6, "symbols": false, "alpha": true, "casing": "mixed"}},
-                "card_verification_code": {"$string": {"length": 4, "symbols": false, "alpha": true, "casing": "upper"}},
-                "cardholder_email": "$email",
-                "card_present_indicator": {"$choose": {"from": ["N","Y"],"weights": [4,10] }}
-            },
-            "transaction_details": {
-                "source_code": {"$string": {"length": 1, "symbols": false, "alpha": true, "casing": "upper"}},
-                "location": {"$string": {"length": 3, "symbols": false, "alpha": true, "casing": "upper"}},
-                "type": {"$string": {"length": 10, "symbols": false, "alpha": true, "casing": "upper"}},
-                "transmission_date" : {"$date": {"min": "2020-01-01", "max": "2020-12-31"}},
-                "transmission_time": {"$join": {"array": [{"$number": {"min":0, "max":24}},":",{"$number": {"min":0, "max":60}},":",{"$number": {"min":0, "max":60}}]}},
-                "merchant_tran_ref": {"$number": {"min": 100000, "max": 1000000}},
-                "purchase_amount": {"$floating": { "min": 0, "max": 1000, "fixed": 2 }},
-                "chashback_amount": {"$floating": { "min": 0, "max": 10, "fixed": 2 }},
-                "tip": "$bool",
-                "tip_amount": {"$floating": { "min": 0, "max": 10, "fixed": 2 }}
-            },
-            "store_id": {"$number": {"min": 901010, "max": 902020}},
-            "order_id": {"$number": {"min": 901010, "max": 902020}},
-            "terminal_details": {
-                "terminal_id": {"$number": {"min": 1201010, "max": 3402020}},
-                "ip_address": "$ip",
-                "terminal_curr_code": {"$choose": {"from": ["USD","GBP","EUR"],"weights": [1,10,10] }},
-                "terminal_batch_num": {"$number": {"min": 1201010, "max": 3402020}},
-                "pos_entry": {"$string": {"length": 2, "symbols": false, "alpha": true, "casing": "upper"}},
-                "pos_capability": {"$string": {"length": 3, "symbols": false, "alpha": true, "casing": "lower"}}
-            }
-        })
- }
- 
-function insertData() {
+function log(data) {
+    fs.appendFileSync(fileName,data+"\n", 'utf8');
+    console.log(data)
+}
+
+async function initialize() {
     var myArgs = process.argv.slice(2);
     if (myArgs.length != 2) {
         throw("Need to to supply a URI as the first parameter and number of documents to load as the second")
     }
-    var URI = myArgs[0]
+    URI = myArgs[0]
     if  (typeof URI === "undefined") {
         throw("Need to to supply a URI as the first parameter")
     }
-    var numberOfDocs = myArgs[1]
+    numberOfDocs = myArgs[1]
     if  (typeof numberOfDocs === "undefined") {
         numberOfDocs = 1000
     }
-    if (numberOfDocs > 1000000) {
-        console.log("Max number of docs that can be loaded in this script is 1 million as we're doing it in a bulk operation")
-        return
-    }
-    console.log('numberOfDocs: ', numberOfDocs, 'URI: ', URI);
-    
-    MongoClient.connect(URI, function(err, client) {
-        // Get the collection
-        db = client.db("fiservTest");
+    await getConnection().catch(console.dir);
+} 
 
-            var batch = db.collection('authorisations').initializeUnorderedBulkOp();                
-            for (var i = 0; i < numberOfDocs; ++i) {
-                var auth = generateAuth()
-                batch.insert(auth);
-            }
+async function getConnection() {
+    // Connect the client to the server
+    client = await MongoClient.connect(URI, { useNewUrlParser: true });
 
-            // Execute the operations
-            batch.execute(function(err, result) {
-                if (err) {
-                    console.dir(err);
-                }
-                // console.dir(result);
-                client.close();
-                console.log('executed batch ',result)
-            });
-        if (err) {
-            console.dir(err);
-        }
-    });
+    // Establish and verify connection
+    db = await client.db('fiservTest');
+
+    console.log("Connected successfully to server");
 }
 
-insertData()
+async function generateAuth() {
+    return mgenerate(
+       {
+           "auth_id": "$objectid",
+           "merch_id": {"$number": {"min": 30001, "max": 30002}},
+           "alliance_code" : "CODE123",
+           "bank_name": "bank_name",
+           "bank_country": "$country",
+           "posting_date" : {"$date": {"min": "2020-01-01", "max": "2020-12-31"}},
+           "merchant_details": {
+               "external_merch_id": {"$number": {"min": 100000, "max": 1000000}},
+               "custom_merch_id": {"$number": {"min": 100000, "max": 1000000}},
+               "merch_type": {"$string": {"length": 6, "symbols": false, "alpha": true, "casing": "upper"}},
+               "name": "$company",
+               "trade_name": "$company",
+               "mcc_iso": {"$number": {"min": 100000, "max": 1000000}},
+               "partner_code": {"$join": {"array": [{"$string": {"length": 3, "symbols": false, "alpha": true, "casing": "upper"}}, {"$number": {"min":10000, "max":20000}}]}}
+           },
+           "agent_code": {"$number": {"min": 100000, "max": 1000000}},
+           "platform_code": {"$number": {"min": 100000, "max": 1000000}},
+           "instrument_details": {
+               "instrument_num": {"$number": {"min": 100000, "max": 1000000}},
+               "instrument_num_masked": {"$number": {"min": 1000000000000000, "max": 9000000000000000}},
+               "instrument_type_code" : {"$string": {"length": 3, "symbols": false, "alpha": true, "casing": "upper"}},
+               "instrument_type_subcode" : {"$string": {"length": 3, "symbols": false, "alpha": true, "casing": "upper"}}
+           },
+           "auth_details": {
+               "auth_code" : {"$string": {"length": 9, "symbols": false, "alpha": true, "casing": "upper"}},
+               "auth_date" : {"$date": {"min": "2020-01-01", "max": "2020-12-31"}},
+               "auth_exp" : {"$date": {"min": "2020-01-01", "max": "2020-12-31"}},
+               "auth_flag": "AA",
+               "auth_id": "$objectid",
+               "auth_ref_id": {"$number": {"min": 100000, "max": 1000000}},
+               "auth_seq": {"$number": {"min": 1, "max": 10}},
+               "auth_time": {"$join": {"array": [{"$number": {"min":0, "max":24}},":",{"$number": {"min":0, "max":60}},":",{"$number": {"min":0, "max":60}}]}},
+               "auth_method": {"$choose": {"from": ["ABC","123","XYZ"]}},
+               "auth_method_code": {"$number": {"min": 1021, "max": 2131}},
+               "auth_platform_code": {"$number": {"min": 1021, "max": 2131}}
+           },
+           "acquirer_details" : {
+               "merch_id": {"$number": {"min": 901000, "max": 901030}},
+               "resp_code": {"$number": {"min": 9001, "max": 9999}},
+               "resp_code_desc": "$string",
+               "terminal_id": {"$number": {"min": 301000, "max": 301030}},
+               "acquired_id": {"$number": {"min": 401000, "max": 401030}},
+               "bin": {"$string": {"length": 4, "symbols": false, "alpha": true, "casing": "upper"}},
+               "token": "$objectid"
+           },
+           "account_details": {
+               "account_type": {"$choose": {"from": ["Credit","Debit","chequing"],"weights": [10, 10, 1]}},
+               "account_type_code": {"$choose": {"from": ["CR1213","DB1212","CQ1213"],"weights": [10, 10, 1]}},
+               "account_update_service_flag": {"$string": {"length": 2, "symbols": false, "alpha": true, "casing": "upper"}}
+           },
+           "card_details" : {
+               "card_classification" : {"$string": {"length": 6, "symbols": false, "alpha": true, "casing": "mixed"}},
+               "card_curr_code": {"$choose": {"from": ["USD","GBP","EUR"],"weights": [1,10,10] }},
+               "card_holder_name": "$name",
+               "card_service_type_code": {"$string": {"length": 4, "symbols": false, "alpha": true, "casing": "upper"}},
+               "card_usage_code": {"$string": {"length": 4, "symbols": false, "alpha": true, "casing": "upper"}},
+               "card_verification": {"$string": {"length": 6, "symbols": false, "alpha": true, "casing": "mixed"}},
+               "card_verification_code": {"$string": {"length": 4, "symbols": false, "alpha": true, "casing": "upper"}},
+               "cardholder_email": "$email",
+               "card_present_indicator": {"$choose": {"from": ["N","Y"],"weights": [4,10] }}
+           },
+           "transaction_details": {
+               "source_code": {"$string": {"length": 1, "symbols": false, "alpha": true, "casing": "upper"}},
+               "location": {"$string": {"length": 3, "symbols": false, "alpha": true, "casing": "upper"}},
+               "type": {"$string": {"length": 10, "symbols": false, "alpha": true, "casing": "upper"}},
+               "transmission_date" : {"$date": {"min": "2020-01-01", "max": "2020-12-31"}},
+               "transmission_time": {"$join": {"array": [{"$number": {"min":0, "max":24}},":",{"$number": {"min":0, "max":60}},":",{"$number": {"min":0, "max":60}}]}},
+               "merchant_tran_ref": {"$number": {"min": 100000, "max": 1000000}},
+               "purchase_amount": {"$floating": { "min": 0, "max": 1000, "fixed": 2 }},
+               "chashback_amount": {"$floating": { "min": 0, "max": 10, "fixed": 2 }},
+               "tip": "$bool",
+               "tip_amount": {"$floating": { "min": 0, "max": 10, "fixed": 2 }}
+           },
+           "store_id": {"$number": {"min": 901010, "max": 902020}},
+           "order_id": {"$number": {"min": 901010, "max": 902020}},
+           "terminal_details": {
+               "terminal_id": {"$number": {"min": 1201010, "max": 3402020}},
+               "ip_address": "$ip",
+               "terminal_curr_code": {"$choose": {"from": ["USD","GBP","EUR"],"weights": [1,10,10] }},
+               "terminal_batch_num": {"$number": {"min": 1201010, "max": 3402020}},
+               "pos_entry": {"$string": {"length": 2, "symbols": false, "alpha": true, "casing": "upper"}},
+               "pos_capability": {"$string": {"length": 3, "symbols": false, "alpha": true, "casing": "lower"}}
+           }
+       })
+}
+
+async function insertData() {
+    try {
+        var batch = db.collection('authorisations').initializeUnorderedBulkOp();
+        for (var i = 0; i < BATCH_SIZE; ++i) {
+            var auth = await generateAuth()
+            await batch.insert(auth);
+        }
+        // Execute the operations
+        let res = await batch.execute()
+        // console.log(res);
+    } catch (err) {
+        log(err);
+    }
+}
+
+async function load() {
+    await initialize()
+    var counter = Math.ceil(numberOfDocs/BATCH_SIZE)
+    var currentdate = new Date();
+    fileName = "Dataload-"+currentdate.toString()+".log"
+    log("\nloading "+numberOfDocs+" in "+counter+" batches of "+BATCH_SIZE+" at "+currentdate.toString()+"\n")
+    var before = new Date()
+    for (var j=0; j<counter; j++){
+        let res = await insertData()
+        var after = new Date()
+    }
+    execution_mills = after - before
+    log("Completed loading "+numberOfDocs+" in "+execution_mills+" (ms)")
+await client.close();
+}
+
+load()
--- tests/performace_test.py
+++ tests/performace_test.py
@@ -1,8 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from locust import task, between
 
 from mongo_user import MongoUser
 import random
+import generate_authorisation
 
 # number of cache entries for updates and queries
 IDS_TO_CACHE = 1000000
@@ -19,34 +20,16 @@ class MongoSampleUser(MongoUser):
         self.auth_id_cache = []
         self.authorisation_collection = None
 
-    def generate_new_authorisation(self):
-        """
-        Generate an authorisation document
-        """
-        document = {
-            'auth_id': self.faker.uuid4(),
-            'merch_id': self.faker.random_int(min=901000, max=901030),
-            'alliance_code': self.faker.lexify(text='????', letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-            'bank_name': self.faker.lexify(text='???? PLC.', letters='ABCDEFGHIJKLMNOPQRSTUVWXYZ'),
-            'bank_country': self.faker.country(),
-            'posting_date': self.faker.date_time_between(start_date='-2y', end_date='-1y'),
-            'merchant_detals': {
-                'external_merch_id': self.faker.random_int(min=100000, max=999999),
-                'custom_merch_id': self.faker.random_int(min=100000, max=999999),
-            }
-        }
-        return document
-
     def on_start(self):
         """
         Executed every time a new test is started - place init code here
         """
         # prepare the collection
-        self.authorisation_collection = self.ensure_collection('authorisations', '')
+        self.authorisation_collection = self.ensure_collection('authorisations')
         self.auth_id_cache = []
 
     def insert_authorisation(self):
-        document = self.generate_new_authorisation()
+        document = generate_authorisation.Authorisation.generate_authorisation_doc(3)
 
         self.auth_id_to_cache = (document['auth_id'])
         if len(self.auth_id_cache) < IDS_TO_CACHE:
@@ -65,20 +48,41 @@ class MongoSampleUser(MongoUser):
 
         # find a random document using an the auth id and update it
         auth_id = random.choice(self.auth_id_cache)
-        # self.collection.find_one({'first_name': cached_names[0], 'last_name': cached_names[1]})
         self.authorisation_collection.update_one({'auth_id': auth_id}, {
-                '$currentDate': {'updates.last_updated': { '$type': 'date' },
-                '$set': {'updates.some_field': 'some_value'}}
+                '$currentDate': {'updates.last_updated': { '$type': 'date' }},
+                '$set': {'updates.some_field': 'some_value'}
                 })
 
     def find_authorisation(self):
-        # Find a random auth id to update
-        if not self.auth_id_cache:
-            return
+        # Find a random day & time to search
+        day = random.randint(1, 30)
+        start_hr = random.randint(0, 22)
+        end_hr = start_hr+1
+
+        filter={
+            'merch_id': 30001, 
+            'alliance_code': 'CODE123', 
+            'posting_date': {
+                '$gte': datetime(2021, 6, day, start_hr, 0, 0, tzinfo=timezone.utc), 
+                '$lte': datetime(2021, 6, day, end_hr, 0, 0, tzinfo=timezone.utc)
+            }
+        }
+        sort=list({
+            'posting_date': -1
+        }.items())
+        skip=0
+        limit=10
 
         # find a random document using an the auth id and update it
-        auth_id = random.choice(self.auth_id_cache)
-        self.authorisation_collection.find_one({'auth_id': auth_id})
+        return list(self.authorisation_collection.find(
+            filter=filter,
+            sort=sort,
+            skip=skip,
+            limit=limit
+        ))
+
+
+
 
     @task(weight=1)
     def do_find_authorisation(self):
