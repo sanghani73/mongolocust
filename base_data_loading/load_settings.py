1d280c9e169dce59531e3cf8631d51c295e6c1aa
Anand Sanghani <>
2021-11-02 10:29:43 +0000

    Tidied up

--- base_data_loading/load_settings.py
+++
@@ -1,9 +0,0 @@
-# Database name to use
-DB_NAME = 'fiservTest'
-
-# cluster connection string
-CLUSTER_URL = f'mongodb+srv://user:password@something.mongodb.net/{DB_NAME}?retryWrites=true&w=majority'
-
-
-# Weighted processing
-PROCESSES = 10
